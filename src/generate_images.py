c92e16c1b51bdc1e772a5a57f5f78e1287171264
Jorge Brito <jorgelosb@gmail.com>
2024-01-29 22:19:02 -0300

    update train model and generate images code

--- src/generate_images.py
+++ src/generate_images.py
@@ -1,49 +1,70 @@
 import numpy as np
 from PIL import Image
-from pathlib import Path
 import os
 import re
+from pathlib import Path
+import random
 
-def convert_can_log_to_byteplot(dataset_path, output_folder):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    
-    extensions = ['txt']
+def hex_to_binary(hex_data):
+    binary_data = bin(int(hex_data, 16))[2:].zfill(len(hex_data)*4)
+    return [int(b) for b in binary_data]
 
-    for filename in os.listdir(dataset_path):
-        file_path = os.path.join(dataset_path, filename)
-        all_bytes = []
+def convert_message_to_byteplot(message, output_folder, file_count):
+    match = re.search(r'DLC: \d+    ([0-9a-fA-F ]+)', message)
+    if match:
+        hex_data = match.group(1).replace(' ', '')
+        binary_data = hex_to_binary(hex_data)
 
-        extension = filename.split('.')[-1]
-        if extension not in extensions:
-            continue
-
-        with open(file_path, 'r') as file:
-            for line in file:
-                match = re.search(r'DLC: \d+    ([0-9a-fA-F ]+)', line)
-                if match:
-                    hex_data = match.group(1).replace(' ', '')
-                    bytes_data = bytes.fromhex(hex_data)
-                    all_bytes.extend(bytes_data)
-
-        byte_array = np.array(all_bytes, dtype=np.uint8)
-        size = int(np.ceil(np.sqrt(len(byte_array))))
-        if size*size < len(byte_array):
+        byte_image = np.array(binary_data, dtype=np.uint8).reshape(-1, 1)
+        size = int(np.ceil(np.sqrt(len(byte_image))))
+        if size*size < len(byte_image):
             size += 1
 
-        byte_image = np.zeros((size*size,), dtype=np.uint8)
-        byte_image[:len(byte_array)] = byte_array
-        byte_image = np.reshape(byte_image, (size, size))
+        byte_image = np.pad(byte_image, ((0, size*size - len(byte_image)), (0, 0)), 'constant')
 
-        gray_image = Image.fromarray(byte_image, 'L')
-
+        gray_image = Image.fromarray(byte_image * 255, 'L').resize((size, size))
         rgb_image = gray_image.convert('RGB')
-
         resized_image = rgb_image.resize((224, 224), Image.Resampling.LANCZOS)
 
-        resized_image.save(os.path.join(output_folder, f'{filename}.png'))
+        image_save_path = os.path.join(output_folder, f'{file_count}.png')
+        resized_image.save(image_save_path)
 
+def split_dataset(dataset_path, train_ratio=0.7):
+    with open(dataset_path, 'r') as file:
+        lines = file.readlines()
+
+    random.shuffle(lines)
+    split_index = int(len(lines) * train_ratio)
+    return lines[:split_index], lines[split_index:]
+
+def process_dataset(dataset_path, train_folder, validation_folder):
+    train_lines, val_lines = split_dataset(dataset_path)
+
+    if not os.path.exists(train_folder):
+        os.makedirs(train_folder)
+    if not os.path.exists(validation_folder):
+        os.makedirs(validation_folder)
+
+    for i, line in enumerate(train_lines):
+        convert_message_to_byteplot(line, train_folder, i)
+
+    for i, line in enumerate(val_lines):
+        convert_message_to_byteplot(line, validation_folder, i)
+
+def process_all_datasets(datasets_folder, output_base_folder):
+    for filename in os.listdir(datasets_folder):
+        if filename.endswith('.txt'):
+            dataset_path = os.path.join(datasets_folder, filename)
+            dataset_name = os.path.splitext(filename)[0]
+
+            train_folder = os.path.join(output_base_folder, 'train', dataset_name)
+            validation_folder = os.path.join(output_base_folder, 'validation', dataset_name)
+
+            process_dataset(dataset_path, train_folder, validation_folder)
+
+# Caminho para a pasta com os datasets e a pasta base para as imagens
 path = Path(os.getcwd())
-data_dir = str(path) + "/data/"
+datasets_folder = str(path) + "/data/datasets"
+output_base_folder = str(path) + "/data"
 
-convert_can_log_to_byteplot(data_dir + 'datasets', data_dir + 'images')
+process_all_datasets(datasets_folder, output_base_folder)
--- src/train_model.py
+++ src/train_model.py
@@ -1,24 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import models, transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, Subset
+from torchvision.models import shufflenet_v2_x1_0, ShuffleNet_V2_X1_0_Weights
 from pathlib import Path
 import os
-from torchvision.models import shufflenet_v2_x1_0, ShuffleNet_V2_X1_0_Weights
+import numpy as np
 
 path = Path(os.getcwd())
 data_dir = str(path) + "/data/"
-
-train_dir = data_dir + 'images'
-val_dir = data_dir + 'validation'
+train_dir = data_dir + 'train/'
+val_dir = data_dir + 'validation/'
 
 num_classes = 3
 batch_size = 32
 epochs = 10
+use_gpu = True
+train_data_percentage = 0.01
 
-base_model = shufflenet_v2_x1_0(weights=ShuffleNet_V2_X1_0_Weights.IMAGENET1K_V1)
+device = torch.device("cuda:0" if torch.cuda.is_available() and use_gpu else "cpu")
+
+base_model = shufflenet_v2_x1_0(weights=ShuffleNet_V2_X1_0_Weights.IMAGENET1K_V1).to(device)
 for param in base_model.parameters():
     param.requires_grad = False
 
@@ -34,10 +37,10 @@ class CustomShuffleNet(nn.Module):
         x = self.new_fc(x.view(x.size(0), -1))
         return x
 
-base_model = CustomShuffleNet(base_model, num_classes)
+model = CustomShuffleNet(base_model, num_classes).to(device)
 
+optimizer = optim.Adam(model.new_fc.parameters(), lr=0.001)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(base_model.new_fc.parameters(), lr=0.001)
 
 train_transforms = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -51,33 +54,44 @@ val_transforms = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
-train_data = ImageFolder(train_dir, transform=train_transforms)
-val_data = ImageFolder(val_dir, transform=val_transforms)
+def load_data(data_dir, transforms, percentage='*'):
+    dataset = datasets.ImageFolder(data_dir, transform=transforms)
+    if percentage == '*':
+        return dataset
+    else:
+        subset_size = int(len(dataset) * percentage)
+        indices = np.random.choice(range(len(dataset)), subset_size, replace=False)
+        return Subset(dataset, indices)
 
-train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_data, batch_size=batch_size)
+train_data = load_data(train_dir, train_transforms, train_data_percentage)
+val_data = load_data(val_dir, val_transforms, train_data_percentage)
 
-for epoch in range(epochs):
-    base_model.train()
-    for inputs, labels in train_loader:
-        inputs, labels = inputs.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu')), labels.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
-        optimizer.zero_grad()
-        outputs = base_model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=4, prefetch_factor=2)
+val_loader = DataLoader(val_data, batch_size=batch_size, num_workers=4, prefetch_factor=2)
 
-    base_model.eval()
-    total = 0
-    correct = 0
-    with torch.no_grad():
-        for inputs, labels in val_loader:
-            inputs, labels = inputs.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu')), labels.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
-            outputs = base_model(inputs)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+def train_and_validate(model, epochs, train_loader, val_loader, criterion, optimizer, device):
+    for epoch in range(epochs):
+        model.train()
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-    print(f'Epoch {epoch+1}, Val Accuracy: {100 * correct / total}%')
+        model.eval()
+        total, correct = 0, 0
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-torch.save(base_model.state_dict(), data_dir+'trained/result.pth')
+        print(f'Epoch {epoch+1}, Val Accuracy: {100 * correct / total}%')
+
+train_and_validate(model, epochs, train_loader, val_loader, criterion, optimizer, device)
+
+torch.save(model.state_dict(), data_dir+'result/result.pth')
