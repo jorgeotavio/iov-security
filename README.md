dbfa86f9426b2999e11c0f2795d5d7752dd3720c
Jorge Otávio <jorgelosb@gmail.com>
2024-04-30 00:30:04 -0300

    update env

---
+++ README.md
@@ -0,0 +1,20 @@
+# Iov IDS
+
+To run this project, you will need create a virtual env of python or run with docker.
+
+### To run with virtual env
+
+Please, use the cmd prompt, if you are using Windows.
+
+run: `python -m venv env`
+run wind cmd: `env\Scripts\activate.bat`
+run: `pip install --upgrade pip`
+run: `pip install --no-cache-dir -r requirements.txt`
+
+### To run with Docker
+
+run: `sudo docker-compose up`
+
+OR
+
+run: `sudo docker compose up`
--- src/generate_images.py
+++ src/generate_images.py
@@ -4,79 +4,81 @@ import os
 import re
 from pathlib import Path
 import random
-# from multiprocessing import Process, cpu_count
-import threading
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm import tqdm
 
 def hex_to_binary(hex_data):
     binary_data = bin(int(hex_data, 16))[2:].zfill(len(hex_data)*4)
     return [int(b) for b in binary_data]
 
 def convert_message_to_byteplot(message, output_folder, file_count):
-    match = re.search(r'DLC: \d+    ([0-9a-fA-F ]+)', message)
-    if match:
-        hex_data = match.group(1).replace(' ', '')
-        binary_data = hex_to_binary(hex_data)
+    hex_data = message.replace(' ', '')
+    binary_data = hex_to_binary(hex_data)
 
-        byte_image = np.array(binary_data, dtype=np.uint8).reshape(-1, 1)
-        size = int(np.ceil(np.sqrt(len(byte_image))))
-        if size*size < len(byte_image):
-            size += 1
+    byte_image = np.array(binary_data, dtype=np.uint8).reshape(-1, 1)
+    size = int(np.ceil(np.sqrt(len(byte_image))))
+    if size * size < len(byte_image):
+        size += 1
 
-        byte_image = np.pad(byte_image, ((0, size*size - len(byte_image)), (0, 0)), 'constant')
+    byte_image = np.pad(byte_image, ((0, size * size - len(byte_image)), (0, 0)), 'constant')
 
-        gray_image = Image.fromarray(byte_image * 255, 'L').resize((size, size))
-        rgb_image = gray_image.convert('RGB')
-        resized_image = rgb_image.resize((224, 224), Image.Resampling.LANCZOS)
+    gray_image = Image.fromarray(byte_image * 255, 'L').resize((size, size))
+    rgb_image = gray_image.convert('RGB')
+    resized_image = rgb_image.resize((224, 224), Image.Resampling.LANCZOS)
 
-        image_save_path = os.path.join(output_folder, f'{file_count}.png')
-        resized_image.save(image_save_path)
+    image_save_path = os.path.join(output_folder, f'{file_count}.png')
+    resized_image.save(image_save_path)
 
-def split_dataset(dataset_path, train_ratio=0.7):
+def split_dataset(dataset_path):
     with open(dataset_path, 'r') as file:
-        lines = file.readlines()
+        regex = r'DLC: \d+    ([0-9a-fA-F ]+)'
+        lines = [
+            re.search(regex, line).group(1) for line in file.readlines() if re.search(regex, line)
+        ]
 
     random.shuffle(lines)
-    split_index = int(len(lines) * train_ratio)
+    size_of_dataset = int(len(lines) * 0.1)
+    lines = lines[:size_of_dataset]
+
+    split_index = int(len(lines) * 0.7)
     return lines[:split_index], lines[split_index:]
 
-def process_lines(lines, train_folder):
+def process_lines(lines, train_folder, progress_bar):
     for i, line in enumerate(lines):
         convert_message_to_byteplot(line, train_folder, i)
+        progress_bar.update(1)
 
-def process_dataset(dataset_path, train_folder, validation_folder):
+def process_dataset(dataset_path, train_folder, validation_folder, progress_bar):
     train_lines, val_lines = split_dataset(dataset_path)
+
+    print("Dataset: ", dataset_path)
+    print("Total Train Lines: ", len(train_lines))
+    print("Total Validate Lines: ", len(val_lines))
 
     if not os.path.exists(train_folder):
         os.makedirs(train_folder)
     if not os.path.exists(validation_folder):
         os.makedirs(validation_folder)
 
-    # train_process = Process(target=process_lines, args=(train_lines, train_folder))
-    # val_process = Process(target=process_lines, args=(val_lines, validation_folder))
-    # train_process.start()
-    # val_process.start()
-    # train_process.join()
-    # val_process.join()
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(process_lines, train_lines, train_folder, tqdm(total=len(train_lines), desc="Processing train lines")),
+                   executor.submit(process_lines, val_lines, validation_folder, tqdm(total=len(val_lines), desc="Processing validation lines"))]
+        for future in as_completed(futures):
+            future.result()
 
-    train_line = threading.Thread(target=process_lines, args=(train_lines, train_folder))
-    val_line = threading.Thread(target=process_lines, args=(val_lines, validation_folder))
-    train_line.start()
-    val_line.start()
-
-    
 def process_all_datasets(datasets_folder, output_base_folder):
-    for filename in os.listdir(datasets_folder):
-        if filename.endswith('.txt'):
-            dataset_path = os.path.join(datasets_folder, filename)
-            dataset_name = os.path.splitext(filename)[0]
+    with ProcessPoolExecutor() as executor:
+        for filename in os.listdir(datasets_folder):
+            if filename.endswith('.txt'):
+                dataset_path = os.path.join(datasets_folder, filename)
+                dataset_name = os.path.splitext(filename)[0]
+                train_folder = os.path.join(output_base_folder, 'train', dataset_name)
+                validation_folder = os.path.join(output_base_folder, 'validation', dataset_name)
 
-            train_folder = os.path.join(output_base_folder, 'train', dataset_name)
-            validation_folder = os.path.join(output_base_folder, 'validation', dataset_name)
+                executor.submit(process_dataset, dataset_path, train_folder, validation_folder, tqdm(desc=f"Processing dataset: {dataset_name}", position=0, leave=False))
 
-            process_dataset(dataset_path, train_folder, validation_folder)
+print("Working with this total of CPUs: ", os.cpu_count())
 
-print(os.cpu_count())
-# Caminho para a pasta com os datasets e a pasta base para as imagens
 path = Path(os.getcwd())
 datasets_folder = str(path) + "/data/datasets"
 output_base_folder = str(path) + "/data"
